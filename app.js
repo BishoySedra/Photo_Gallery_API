0b95cd06875a4e318778219d9fd721059aa99fe0
Bishoy_Sedra <93890269+BishoySedra@users.noreply.github.com>
2023-11-05 21:06:35 +0200

    Create_Directory Function

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/connection.js";
 import Photo from "./models/photo.js";
 import Album from "./models/album.js";
-import AlbumOfPhotos from "./models/album_of_photos.js";
+//import AlbumOfPhotos from "./models/album_of_photos.js";
 import photoRoutes from "./routes/photo.js";
 import albumRoutes from "./routes/album.js";
+import * as directoryFunctions from "./helpers/directory.js";
 
 dotenv.config();
 
@@ -23,6 +24,9 @@ app.use(`${baseURL}/photo`, photoRoutes);
 app.use(`${baseURL}/album`, albumRoutes);
 
 connectDB();
+
+// create uploads folder
+directoryFunctions.create_uploads_directory();
 
 try {
     const port = process.env.PORT || 3000;
